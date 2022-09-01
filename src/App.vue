a82dbf9821ed5dd00c516b4cd68e81edff7489b9
Omega <duneomega@gmail.com>
2022-09-02 07:24:52 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,61 @@
+<template>
+  <h1>Hello World</h1>
+  <JobList :jobs="Jobs" :order="order" />
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from "vue";
+import { Job, JobOrder } from "./types/Jobs";
+import JobList from "./components/JobList.vue";
+
+export default defineComponent({
+  name: "App",
+  components: { JobList },
+  setup() {
+    const Jobs = ref<Job[]>([
+      {
+        title: " farm worker ",
+        location: " lon lon ranch ",
+        salary: 30000,
+        id: " 1 ",
+      },
+      {
+        title: " quarryman ",
+        location: " death mountain ",
+        salary: 40000,
+        id: " 2 ",
+      },
+      {
+        title: " flute player ",
+        location: " the lost woods ",
+        salary: 35000,
+        id: " 3 ",
+      },
+      {
+        title: " fisherman ",
+        location: " lake hylia ",
+        salary: 21000,
+        id: " 4 ",
+      },
+      {
+        title: " prison guard ",
+        location: " gerudo valley ",
+        salary: 32000,
+        id: " 5 ",
+      },
+    ]);
+
+    const order = ref<JobOrder>("title");
+
+    const handleOrder = (order: JobOrder) => {
+      console.log(order);
+    };
+
+    handleOrder("id");
+
+    return { Jobs, order };
+  },
+});
+</script>
+
+<style></style>
---
+++ src/components/JobList.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="job-list">
+    <ul>
+      <li v-for="job in jobs" :key="job.id"></li>
+    </ul>
+  </div>
+</template>
+
+<script lang="ts">
+import { Job, JobOrder } from "../types/Jobs";
+import { defineComponent, PropType } from "vue";
+
+export default defineComponent({
+  props: {
+    jobs: {
+      required: true,
+      type: Array as PropType<Job[]>,
+    },
+    order: {
+      required: true,
+      type: String as PropType<JobOrder>,
+    },
+  },
+});
+</script>
+
+<style scoped></style>
